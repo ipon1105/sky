f68c6d0d275f2fdd9c71b997e784b8ab88f7cd88
Konovalov Ivan <my_world1105@mail.ru>
2022-10-19 16:33:48 +0300

    Create AndroidStudio KMM project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "sky"
+include(":androidApp")
+include(":shared")
