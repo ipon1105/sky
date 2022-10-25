de282c951d2963259ee9629c6b336052041528d2
Konovalov Ivan <my_world1105@mail.ru>
2022-10-25 13:24:13 +0300

    Hide System Navigation Bar

--- androidApp/src/main/java/com/example/sky/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/sky/android/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.example.sky.android
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import com.example.sky.navigation.SkyNavHost
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SkyNavHost()
+        }
+
+        // Прячем системную панель навигации
+        window.insetsController?.apply {
+            hide(WindowInsets.Type.navigationBars())
+            systemBarsBehavior =
+                WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         }
     }
 }
