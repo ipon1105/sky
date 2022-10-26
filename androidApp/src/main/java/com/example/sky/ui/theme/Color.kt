db226e248f35f2f2fda6fcfdf5767e13eb03f5de
Konovalov Ivan <my_world1105@mail.ru>
2022-10-26 22:53:48 +0300

    Create files with general data

---
+++ androidApp/src/main/java/com/example/sky/ui/theme/Color.kt
@@ -0,0 +1,6 @@
+package com.example.sky.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val mainColor = Color(0xFF0071BC)
+val linkColor = Color(0xFF0052CC)
---
+++ androidApp/src/main/java/com/example/sky/ui/theme/Shape.kt
@@ -0,0 +1,7 @@
+package com.example.sky.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val lowShape = 3.dp
+val normalShape = 6.dp
+val bigShape = 9.dp
