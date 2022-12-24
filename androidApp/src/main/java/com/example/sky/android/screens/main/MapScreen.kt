832870dce5bfc0599343d9ff4d5914284fdafd8e
ipon1105 <my_world1105@mail.ru>
2022-12-25 02:37:10 +0300

    Add text on mapscreen

--- androidApp/src/main/java/com/example/sky/android/screens/main/MapScreen.kt
+++ androidApp/src/main/java/com/example/sky/android/screens/main/MapScreen.kt
@@ -1,21 +1,22 @@
 package com.example.sky.android.screens
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import com.example.sky.android.R
+import com.example.sky.ui.theme.ComponentDiffNormal
+import com.example.sky.ui.theme.LargeFont
 
 @Composable
 fun MapScreen() {
-    val singapore = LatLng(1.35, 103.87)
-    val cameraPositionState: CameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(singapore, 11f)
-    }
-    Box(Modifier.fillMaxSize()) {
-        GoogleMap(cameraPositionState = cameraPositionState)
-    }
-
+    // Заголовок страницы
+    Text(
+        text = stringResource(id = R.string.map_not_sup),
+        color = Color.White,
+        fontSize = LargeFont,
+        modifier = Modifier.padding(ComponentDiffNormal)
+    )
 }
